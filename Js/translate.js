9b04b8bcc8b040f5dab8291c2ce52a394621edbc
João <joojvitor13@gmail.com>
2026-04-01 23:29:40 -0300

    Update 1.7
    Implementações:
    
    -Troca da imagem de perfil.
    
    -Troca dos currículos para versões mais atualizadas.
    
    -Atualizações no translate.js para que o estilo dos botões de troca de idioma funcione.
    
    -Uma pequena adição de um active para os botões de troca de idioma no components.cs.

--- Js/translate.js
+++ Js/translate.js
@@ -7,9 +7,26 @@ function googleTranslateElementInit() {
 
 function changeLanguage(lang) {
   var select = document.querySelector("select.goog-te-combo");
+
   if (select) {
     select.value = lang;
     select.dispatchEvent(new Event("change"));
+  }
+
+  updateActiveLang(lang);
+}
+
+function updateActiveLang(lang) {
+  const ptBtn = document.getElementById("translate-pt");
+  const enBtn = document.getElementById("translate-en");
+
+  ptBtn.classList.remove("active");
+  enBtn.classList.remove("active");
+
+  if (lang === "pt") {
+    ptBtn.classList.add("active");
+  } else if (lang === "en") {
+    enBtn.classList.add("active");
   }
 }
 
@@ -19,4 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (ptBtn) ptBtn.addEventListener("click", () => changeLanguage("pt"));
   if (enBtn) enBtn.addEventListener("click", () => changeLanguage("en"));
+
+  // idioma padrão
+  updateActiveLang("pt");
 });
